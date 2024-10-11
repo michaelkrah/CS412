ba65c793ded47997ec70b48aa18cba7bee31c75a
michaelkrah <mickra@bu.edu>
2024-10-11 13:17:52 -0400

    Added status messages

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -10,3 +10,25 @@ class Profile(models.Model):
   city = models.TextField(blank=False)
   email_address = models.TextField(blank=False)
   profile_image_url = models.URLField(blank=True)
+
+  def __str__(self):
+    '''return a string representation'''
+    return f"{self.first_name} {self.last_name}"
+  
+  def get_status_messages(self):
+    '''gets all status messages related to this profile'''
+
+    messages = StatusMessage.objects.filter(profile=self)
+    return messages
+  
+
+class StatusMessage(models.Model):
+  '''Describes a status message on a profile'''
+
+  profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+
+  message = models.TextField(blank=False)
+  published = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return f'{self.message}'
